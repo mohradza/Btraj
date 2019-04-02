e4e687799a495f1a3a8314f815cc19741fee5eca
ohrad <ohrad@marble-nuc2.com>
2019-04-01 21:40:12 -0600

    yaw commanding is working in its current form, however replanning based on vehicle status is not working

--- src/b_traj_node.cpp
+++ src/b_traj_node.cpp
@@ -187,7 +187,8 @@ void rcvVehicleStatusCallback(const rosflight_msgs::VehicleStatus status)
 	_vehicle_status = status;
 
 	if (_vehicle_status.replan == 1){
-	    _replan_switch = 1;
+                ROS_INFO("Replan switch");
+    		_replan_switch = 1;
 	}
 
 }
